ad9295554ebe92c89f26a74fb51d544cc5ec12be
sultanofcardio <kevin.lindsay65@gmail.com>
2016-11-14 16:13:25 -0500

    Initial Commit

---
+++ app/src/main/java/com/uwimonacs/computingsociety/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.uwimonacs.computingsociety.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.uwimonacs.computingsociety.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
