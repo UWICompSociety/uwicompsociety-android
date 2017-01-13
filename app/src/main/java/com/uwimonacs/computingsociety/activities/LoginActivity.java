464cc13aa651dede95452a280f77b969d12af768
Doneil <doneilscotland@yahoo.com>
2017-01-13 13:29:25 -0500

    LoginActivity

---
+++ app/src/main/java/com/uwimonacs/computingsociety/activities/LoginActivity.java
@@ -0,0 +1,10 @@
+package com.uwimonacs.computingsociety.activities;
+
+import AppCompatActivity;
+
+/**
+ * Created by Doneil Scotland on 12/01/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+}
